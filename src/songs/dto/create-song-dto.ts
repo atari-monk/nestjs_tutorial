0da6c5f0898bd577d0248631c33c03f46cb80671
atari-monk <atari.monk1@gmail.com>
2024-03-30 17:14:58 +0100

    controller, service, validators

---
+++ src/songs/dto/create-song-dto.ts
@@ -0,0 +1,21 @@
+import { ArrayMinSize, ArrayUnique, IsArray, IsDateString, IsMilitaryTime, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateSongDTO {
+  @IsString()
+  @IsNotEmpty()
+  readonly title: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ArrayUnique()
+  @IsString({ each: true })
+  readonly artists: string[];
+
+  @IsNotEmpty()
+  @IsDateString()
+  readonly releasedDate: Date;
+
+  @IsMilitaryTime()
+  @IsNotEmpty()
+  readonly duration: Date;
+}
--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { SongsService } from './songs.service';
+import { CreateSongDTO } from './dto/create-song-dto';
 
 @Controller('songs')
 export class SongsController {
+  constructor(private songsService: SongsService) {}
+
+  @Post()
+  create(@Body() createSongDTO: CreateSongDTO) {
+    return this.songsService.create(createSongDTO);
+  }
+
   @Get()
   findAll() {
-    return 'find all songs';
+    return this.songsService.findAll();
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'Fetch one song based on id';
+  }
+
+  @Put(':id')
+  update() {
+    return 'Update song based on id';
+  }
+  
+  @Delete(':id')
+  delete() {
+    return 'Delete song based on id';
   }
 }
